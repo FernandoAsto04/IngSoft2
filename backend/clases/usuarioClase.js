a2152de92968cceb21071911044f00d1733c08eb
FernandoAsto04 <20214543@aloe.ulima.edu.pe>
2025-07-02 00:43:23 -0500

    a
    a

--- backend/clases/usuarioClase.js
+++ backend/clases/usuarioClase.js
@@ -1,4 +1,37 @@
 export class UsuarioClase {
+  constructor(usuarioEntidad) {
+    this.id = UsuarioClase.id;
+    this.nombres = UsuarioClase.nombres;
+    this.apellidos = UsuarioClase.apellidos;
+    this.email = UsuarioClase.email;
+    this.contrasenia = UsuarioClase.contrasenia;
+    this.rol = UsuarioClase.rol;
 
-    
+    // Relaciones opcionales
+    this.alumnos = UsuarioClase.Alumnos || [];
+    this.profesores = UsuarioClase.Profesores || [];
+    this.administradores = UsuarioClase.Administradores || [];
+  }
+
+  mostrarDatos() {
+    return {
+      id: this.id,
+      nombres: this.nombres,
+      apellidos: this.apellidos,
+      email: this.email,
+      rol: this.rol
+    };
+  }
+
+  esAdministrador() {
+    return this.rol === 'administrador';
+  }
+
+  esAlumno() {
+    return this.rol === 'alumno';
+  }
+
+  esProfesor() {
+    return this.rol === 'profesor';
+  }
 }
